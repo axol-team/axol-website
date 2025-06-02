ad7a1c8fbdf90ae00c2842974b7015b6d251a969
samj-axol <sam.jordan@axol.team>
2025-06-02 09:46:57 +0100

    Adding GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,18 @@ export default function RootLayout({
           src="https://www.google.com/recaptcha/enterprise.js?render=6Le-f38gAAAAAAgJcEBgn3Y2vqDC5hDGO_u_9YIG"
           strategy="afterInteractive"
         />
+        {/* Google Tag Manager */}
+        <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-PQH3X7HB');
+              `,
+                    }}
+        />
       </head>
       <body
         className={cn(
@@ -74,6 +86,14 @@ export default function RootLayout({
           GeistSans.className
         )}
       >
+        <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-PQH3X7HB"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
